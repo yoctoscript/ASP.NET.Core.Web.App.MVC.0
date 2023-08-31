33449239ad4515aa84bd65b9d4fdf860f5434967
Yoctoscript <yoctoscript@gmail.com>
2023-08-31 15:51:57 -0700

    Chapter 2: Getting Started, Adam Freeman - Pro ASP.NET Core 6

---
+++ MainApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MainApp.Controllers;
+
+public class HomeController : Controller
+{
+    public ViewResult Index()
+    {
+        int hour = DateTime.Now.Hour;
+        string viewModel = hour > 12 ? "Good afternoon" : "Good Morning";
+        return View("MyView", viewModel);
+    }
+
+}
